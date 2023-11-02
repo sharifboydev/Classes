fc20c570b52c648dac2125cab0cb5289d125c92b
sharifboy <143162316+sharifboydev@users.noreply.github.com>
2023-11-02 18:03:03 +0500

    added new code

--- class.py
+++ class.py
@@ -18,3 +18,25 @@
 
 # def get_age(self, yil):
 #     return yil - self.tyil
+
+
+class User:
+    def __init__(self, name, username, email):
+        self.name = name
+        self.uname = username
+        self.mail = email
+
+    def get_name(self):
+        return f"Mening ismim {self.name}"
+
+    def get_user(self):
+        return f"Mening usernamemim {self.uname}"
+
+    def get_email(self):
+        return f"Mening emailim {self.mail}"
+
+
+malumot = User("Anvar", "Anvarovich", "anvar@gmail.com")
+print(malumot.name)
+print(malumot.uname)
+print(malumot.mail)
