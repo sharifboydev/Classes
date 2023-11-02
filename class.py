3ce4ff87294aed20e3e0901972667eceaa75397a
sharifboy <143162316+sharifboydev@users.noreply.github.com>
2023-11-02 17:43:06 +0500

    added new code

---
+++ class.py
@@ -0,0 +1,20 @@
+# class Talaba:
+# def __init__(self, ism, familiya, tyil):
+#     self.ism = ism
+# self.familiya = familiya
+# self.tyil = tyil
+
+# def tanishtir(self):
+# print(f"Ismim {self.ism} {self.familiya}. {self.tyil} yilda tugilganman")
+
+# def get_name(self):
+# return self.ism
+
+# def get_lastname(self):
+# return self.familiya
+
+# def full_name(self):
+#     return f"{self.ism} {self.familiya}"
+
+# def get_age(self, yil):
+#     return yil - self.tyil
