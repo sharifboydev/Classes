e0344aa65c8d10993fd220323eed7cb92009d3d6
sharifboy <143162316+sharifboydev@users.noreply.github.com>
2023-11-07 16:35:20 +0500

    added new code

--- class.py
+++ class.py
@@ -20,23 +20,41 @@
 #     return yil - self.tyil
 
 
-class User:
-    def __init__(self, name, username, email):
-        self.name = name
-        self.uname = username
-        self.mail = email
+# class User:
+#     def __init__(self, name, username, email):
+#         self.name = name
+#         self.uname = username
+#         self.mail = email
+#
+#     def get_name(self):
+#         return f"Mening ismim {self.name}"
+#
+#     def get_user(self):
+#         return f"Mening usernamemim {self.uname}"
+#
+#     def get_email(self):
+#         return f"Mening emailim {self.mail}"
+#
+#
+# malumot = User("Anvar", "Anvarovich", "anvar@gmail.com")
+# print(malumot.name)
+# print(malumot.uname)
+# print(malumot.mail)
 
-    def get_name(self):
-        return f"Mening ismim {self.name}"
 
-    def get_user(self):
-        return f"Mening usernamemim {self.uname}"
+class Shaxs:
+    def __init__(self, ism, familiya, passport, tyil):
+        self.ism = ism
+        self.familiya = familiya
+        self.passport = passport
+        self.tyil = tyil
+    def get_inf0(self):
+        info = f"{self.ism} {self.familiya}."
+        info += f"Passport:{self.passport}, {self.tyil} - yilda tug'ilgan"
+        return info
 
-    def get_email(self):
-        return f"Mening emailim {self.mail}"
+    def get_age(self,yil):
+        return yil -self.tyil
 
-
-malumot = User("Anvar", "Anvarovich", "anvar@gmail.com")
-print(malumot.name)
-print(malumot.uname)
-print(malumot.mail)
+inson = Shaxs("Hasan", "Alimov", "FB001122", 1995)
+print(f"{inson.get_inf0()}. {inson.get_age(2023)} yoshda")
