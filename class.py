18f79f393a91655a73bf0e0eb2176da1d3bd521d
sharifboy <143162316+sharifboydev@users.noreply.github.com>
2023-11-07 16:42:03 +0500

    changes code

--- class.py
+++ class.py
@@ -48,13 +48,23 @@ class Shaxs:
         self.familiya = familiya
         self.passport = passport
         self.tyil = tyil
-    def get_inf0(self):
+
+    def get_info(self):
         info = f"{self.ism} {self.familiya}."
         info += f"Passport:{self.passport}, {self.tyil} - yilda tug'ilgan"
         return info
 
-    def get_age(self,yil):
-        return yil -self.tyil
+    def get_age(self, yil):
+        return yil - self.tyil
 
-inson = Shaxs("Hasan", "Alimov", "FB001122", 1995)
-print(f"{inson.get_inf0()}. {inson.get_age(2023)} yoshda")
+
+# inson = Shaxs("Hasan", "Alimov", "FB001122", 1995)
+# print(f"{inson.get_inf0()}. {inson.get_age(2023)} yoshda")
+
+class Talaba(Shaxs):
+    def __init__(self, ism, familiya, passport, tyil):
+        super().__init__(ism, familiya, passport, tyil)
+
+
+talaba = Talaba("Valijon", "Aliyev", "FA112299", 2000)
+print(talaba.get_age(2023))
